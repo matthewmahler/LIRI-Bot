ea3dfd673b0f6e4e0c5691d1ddfc1483747e1efa
matthewmahler <matthewjamesmahler@gmail.com>
2018-04-09 09:21:09 -0400

    done

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
 
 //============//
 //Conditionals//
@@ -8,28 +9,46 @@ var keys = require("./keys.js");
 
 if (process.argv[2] === "my-tweets") {
   getMyTweets();
-}
-else if (process.argv[2] === "spotify-this-song") {
+} else if (process.argv[2] === "spotify-this-song") {
   spotifyThis();
-}
-else if (process.argv[2] === "movie-this") {
+} else if (process.argv[2] === "movie-this") {
   searchMovie();
-}
-else {
+} else if (process.argv[2] === "do-stuff") {
   doStuff();
 };
+
+//========//
+//Do Stuff//
+//========//
+
+function doStuff() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    var dataArr = data.split(",");
+    console.log(dataArr[0])
+    if (error) {
+      return console.log(error);
+    }
+    if (dataArr[0] === "my-tweets") {
+      getMyTweets(process.argv[3] = dataArr[1]);
+    } else if (dataArr[0] === "spotify-this-song") {
+      spotifyThis(process.argv[3] = dataArr[1]);
+    } else if (dataArr[0] === "movie-this") {
+      searchMovie(process.argv[3] = dataArr[1]);
+    }
+  });
+}
 
 //===============//
 // Movie Requests//
 //===============//
+
 function searchMovie() {
   var nodeArgs = process.argv;
   var movieName = "";
   for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
       movieName = movieName + "+" + nodeArgs[i];
-    }
-    else {
+    } else {
       movieName += nodeArgs[i];
     }
   }
@@ -38,21 +57,15 @@ function searchMovie() {
   request(queryUrl, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       console.log(JSON.parse(body));
+      logEverything(body);
     }
   });
-};
-
-function spotifyThis() {
-  console.log(process.argv[2]);
-};
-
-function doStuff() {
-  console.log(process.argv[2]);
 };
 
 //==============//
 //Twitter Things//
 //==============//
+
 function getMyTweets() {
   var Twitter = require('twitter');
   var client = new Twitter({
@@ -61,22 +74,20 @@ function getMyTweets() {
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
-
   var name = process.argv[3]
-
-  var params = { screen_name: name };
+  console.log(name)
+  var params = {
+    screen_name: name
+  };
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
-
-
       for (var i = 0; i < 20; i++) {
-
         console.log(tweets[i].created_at)
         console.log(" ")
         console.log(tweets[i].text)
         console.log("==================================================================")
-      }
-
+        logEverything(tweets[i]);
+      }      
     }
   });
 }
@@ -84,6 +95,7 @@ function getMyTweets() {
 //==============//
 //Spotify Things//
 //==============//
+
 function spotifyThis() {
   var Spotify = require('node-spotify-api');
 
@@ -91,16 +103,17 @@ function spotifyThis() {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
   });
-
   var trackName = process.argv[3]
-
-  spotify.search({ type: 'track', query: trackName }, function (err, data) {
+  spotify.search({
+    type: 'track',
+    query: trackName
+  }, function (err, data) {
     if (err) {
       console.log('Error occurred: ' + err);
       return;
-    }
-    else {
+    } else {
       var songInfo = data.tracks.items[0];
+      console.log(" ")
       console.log(songInfo.artists[0].name)
       console.log(" ")
       console.log(songInfo.name)
@@ -110,6 +123,32 @@ function spotifyThis() {
       console.log(songInfo.album.name)
       console.log(" ")
       console.log("==================================================================")
+      logEverything(songInfo);
     };
+  });
+  
+}
+
+//==============//
+//Log Everything//
+//==============//
+
+//this part doesnt really work yet
+
+function logEverything(body, tweets, songInfo) {
+  if (process.argv[2] === "my-tweets") {
+   var masterLog = tweets
+  } else if (process.argv[2] === "spotify-this-song") {
+    var masterLog = songInfo
+  } else if (process.argv[2] === "movie-this") {
+    var masterLog = body
+  } 
+  fs.appendFile("log.txt", masterLog, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log("Logged");
+    }
   });
 }
